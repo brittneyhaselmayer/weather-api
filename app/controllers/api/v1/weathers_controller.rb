8437fe1ce2d23ab1312982c8512261bb08a0e58c
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2020-01-17 13:37:14 -0600

    excon gem install

--- app/controllers/api/v1/weathers_controller.rb
+++ app/controllers/api/v1/weathers_controller.rb
@@ -3,16 +3,36 @@ class Api::V1::WeathersController < ApplicationController
     def index()
         # type = params["current"]
         # location = params["94040"]
-        @weather = find_weather(type, location)
+        @weather = find_weather()
+        # byebug
         render json: @weather
+    end
 
-
+    def request_api(url)
+        response = Excon.get(
+            url,
+            headers: {
+                'X-RapidAPI-Host' => URI.parse(url).host
+                # 'X-RapidAPI-Key' => "a58a0e27e4df6f0212f846af7a8adba8"
+            }
+        )
+        # byebug
+        return nil if response.status != 200
+        JSON.parse(response.body)
     end
 
     def find_weather(type="current", location="94040")
     
-    request_api("http://api.openweathermap.org/data/2.5/weather?zip=#{location},us&appid=a58a0e27e4df6f0212f846af7a8adba8")
-
-    end
+        case type
+        when "current"
+        request_api("http://api.openweathermap.org/data/2.5/weather?zip=#{location},us&appid=a58a0e27e4df6f0212f846af7a8adba8")
+        when "hourly"
+            request_api("http://api.openweathermap.org/data/2.5/weather?zip=#{location},us")
+        when "daily"
+            request_api("http://api.openweathermap.org/data/2.5/weather?zip=#{location},us")
+        else
+            request_api("http://api.openweathermap.org/data/2.5/weather?zip=#{location},us")
+        end
+    end 
 
 end
